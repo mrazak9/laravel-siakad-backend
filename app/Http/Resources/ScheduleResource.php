01bbfc47f4451ec574376102c6f8801c83cfe54d
mrazak9 <muhamadrozak9@gmail.com>
2023-10-05 04:01:51 +0700

    edit schedule resource, show lecturer on schedules

--- app/Http/Resources/ScheduleResource.php
+++ app/Http/Resources/ScheduleResource.php
@@ -14,6 +14,33 @@ class ScheduleResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'student_id' => $this->student_id,
+            'subject_id' => $this->subject_id,
+            'schedule_date' => $this->schedule_date,
+            'schedule_type' => $this->schedule_type,
+            'subject' => [
+                'id' => $this->subject->id,
+                'title' => $this->subject->title,
+                'lecturer_id' => $this->subject->lecturer_id,
+                'lecturer' => [
+                    'id' => $this->subject->lecturer->id,
+                    'name' => $this->subject->lecturer->name,
+                    'email' => $this->subject->lecturer->email,
+                    'roles' => $this->subject->lecturer->roles,
+                    'phone' => $this->subject->lecturer->phone,
+                    'address' => $this->subject->lecturer->address,
+                ],
+            ],
+            'student' => [
+                'id' => $this->student->id,
+                'name' => $this->student->name,
+                'email' => $this->student->email,
+                'roles' => $this->student->roles,
+                'phone' => $this->student->phone,
+                'address' => $this->student->address,
+            ],
+        ];
     }
 }
